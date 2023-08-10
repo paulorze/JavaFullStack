bab4581fa6437c0eca439fc849ad5c2f3744269d
pauloRzeszut <agustinrzeszut@hotmail.com>
2023-08-10 18:58:34 -0300

    JS - M7D2E2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 // Llamamos a todos los objetos del HTML que utilizaremos
-const edadInput = document.getElementById('edad');
-const btnCalcular = document.getElementById('btnCalcular');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const btnLogin = document.getElementById('btnLogin');
 const result = document.getElementById('result');
-const emsg1 = document.getElementById('emsg1');
 
-// Creamos las variables que seran utilizadas como flag 
-let edadValue = null;
+// Creamos las variables correspondientes a las credenciales
+const usernameDB = 'admin';
+const passwordDB = '1234';
+
+// Creamos las variables que seran utilizadas como flag
+let usernameValid = false;
+let passwordValid = false;
 
 
 // Creamos las funciones necesarias para validar los datos ingresados y escribir en el HTML
@@ -13,35 +18,26 @@ const write = (obj, text) => {
     obj.innerHTML = text;
 };
 
-const validateInput = (input, emsg) => {
-    const value = Number(input.value);
-    if (!value || value === NaN || value <= 0 || value >= 100) {
-        write(emsg,"Por favor, ingrese un dato valido.");
-        return null 
+const validateInput = (input, flag) => {
+    const value = input.value;
+    if (!value || value !== flag) {
+        return false 
     } else {
-        write(emsg,""); 
-        return value;
+        return true;
     };
 };
 
 const validateForm = () => {
-    edadValue = validateInput(edadInput, emsg1);
-    write(result,"")
-};
-
-const calculate = () => {
-    if (edadValue >= 18) {
-        write(result,"Edad validada, es mayor de 18 anios.")
-    } else {
-        write(result,"Error, es menor de 18 anios.")
-    };
+    usernameValid = validateInput(usernameInput, usernameDB);
+    passwordValid = validateInput(passwordInput, passwordDB);
 };
 
 const send= ()=> {
     validateForm();
-    if (!edadValue) {
+    if (!usernameValid || !passwordValid) {
+        write(result, "Username o password invalidos")
         return
     } else {
-        calculate();
+        write(result, "Bienvenido de nuevo usuario")
     };
 };
