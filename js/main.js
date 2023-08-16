73b9d0f904b0cdd515febb9ab4fd6cb592ceb6a7
pauloRzeszut <agustinrzeszut@hotmail.com>
2023-08-16 17:42:26 -0300

    bugfix: JSM8D1E2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 // Llamamos a todos los objetos del HTML que utilizaremos
 const capital1 = document.getElementById('capital1');
+const plazo1 = document.getElementById('plazo1');
+const tasa1 = document.getElementById('tasa1');
 const interes1 = document.getElementById('interes1');
 const resultante1 = document.getElementById('resultante1');
 const capital2 = document.getElementById('capital2');
+const plazo2 = document.getElementById('plazo2');
+const tasa2 = document.getElementById('tasa2');
 const interes2 = document.getElementById('interes2');
 const resultante2 = document.getElementById('resultante2');
 const capital3 = document.getElementById('capital3');
+const plazo3 = document.getElementById('plazo3');
+const tasa3 = document.getElementById('tasa3');
 const interes3 = document.getElementById('interes3');
 const resultante3 = document.getElementById('resultante3');
 
@@ -88,4 +94,35 @@ const calcular= ()=> {
             });
         });
     }
+};
+
+const constructor = (id, capital, plazo, tasa) => {
+    return {
+        id: id, 
+        capital: +capital.textContent,
+        plazo: +plazo.textContent,
+        tasa: +tasa.textContent,
+        interes: +capital.textContent * +plazo.textContent * +tasa.textContent / 100
+    }
+};
+
+const setData = ()=> {
+    const data = []
+    const plan001 = constructor('Plan 001', capital1, plazo1, tasa1);
+    data.push(plan001);
+    const plan002 = constructor('Plan 002', capital2, plazo2, tasa2);
+    data.push(plan002);
+    const plan003 = constructor('Plan 003', capital3, plazo3, tasa3);
+    data.push(plan003);
+    return data;
+}
+
+const getData = () => {
+    validateData();
+    if (!capitalValid) {
+        write(errorMSG, 'Por favor, verifique los datos ingresados.')
+    } else {
+        const data = setData();
+        console.log(data);
+    };
 };
